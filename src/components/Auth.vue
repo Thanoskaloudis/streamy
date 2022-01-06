973025275ce194f4867b3446bd64f53686f31e1a
Thanoskaloudis <8anos.kal@gmail.com>
2022-01-06 16:04:40 +0200

    init form validation(vee-validate)

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -5,46 +5,36 @@
     :class="{ hidden: !authModalShow }"
   >
     <div
-      class="
-        flex
-        items-end
-        justify-center
-        min-h-screen
-        pt-4
-        px-4
-        pb-20
-        text-center
-        sm:block sm:p-0
-      "
+      class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
     >
-      <div class="fixed inset-0 transition-opacity">
-        <div class="absolute inset-0 bg-gray-800 opacity-75"></div>
+      <div
+        class="fixed inset-0 transition-opacity"
+      >
+        <div
+          class="absolute inset-0 bg-gray-800 opacity-75"
+        ></div>
       </div>
 
       <!-- This element is to trick the browser into centering the modal contents. -->
-      <span class="hidden sm:inline-block sm:align-middle sm:h-screen"
+      <span
+        class="hidden sm:inline-block sm:align-middle sm:h-screen"
         >&#8203;</span
       >
 
       <div
-        class="
-          inline-block
-          align-bottom
-          bg-white
-          rounded-lg
-          text-left
-          overflow-hidden
-          shadow-xl
-          transform
-          transition-all
-          sm:my-8 sm:align-middle sm:max-w-lg sm:w-full
-        "
+        class="inline-block align-bottom bg-white
+         rounded-lg text-left overflow-hidden shadow-xl
+          transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
       >
         <!-- Add margin if you want to see some of the overlay behind the modal-->
         <div class="py-4 text-left px-6">
           <!--Title-->
-          <div class="flex justify-between items-center pb-4">
-            <p class="text-2xl font-bold">Your Account</p>
+          <div
+            class="flex justify-between items-center pb-4"
+          >
+            <p class="text-2xl font-bold">
+              Your Account
+            </p>
             <!-- Modal Close Button -->
             <div
               class="modal-close cursor-pointer z-50"
@@ -57,22 +47,30 @@
           <!-- Tabs -->
           <ul class="flex flex-wrap mb-4">
             <li class="flex-auto text-center">
-              <a class="block rounded py-3 px-4 transition"
-                href="#" @click.prevent="tab = 'login'"
+              <a
+                class="block rounded py-3 px-4 transition"
+                href="#"
+                @click.prevent="tab = 'login'"
                 :class="{
-                  'hover:text-white text-white bg-blue-600' : tab === 'login',
-                  'hover:text-blue-600' : tab === 'register'
-                  }"
+                  'hover:text-white text-white bg-blue-600':
+                    tab === 'login',
+                  'hover:text-blue-600':
+                    tab === 'register',
+                }"
                 >Login</a
               >
             </li>
             <li class="flex-auto text-center">
-              <a class="block rounded py-3 px-4 transition hover:text-blue-600"
-                href="#" @click.prevent="tab = 'register'"
+              <a
+                class="block rounded py-3 px-4 transition hover:text-blue-600"
+                href="#"
+                @click.prevent="tab = 'register'"
                 :class="{
-                  'hover:text-white text-white bg-blue-600' : tab === 'register',
-                  'hover:text-blue-600' : tab === 'login'
-                  }"
+                  'hover:text-white text-white bg-blue-600':
+                    tab === 'register',
+                  'hover:text-blue-600':
+                    tab === 'login',
+                }"
                 >Register</a
               >
             </li>
@@ -82,182 +80,125 @@
           <form v-show="tab === 'login'">
             <!-- Email -->
             <div class="mb-3">
-              <label class="inline-block mb-2">Email</label>
+              <label class="inline-block mb-2"
+                >Email</label
+              >
               <input
                 type="email"
-                class="
-                  block
-                  w-full
-                  py-1.5
-                  px-3
-                  text-gray-800
-                  border border-gray-300
-                  transition
-                  duration-500
-                  focus:outline-none focus:border-black
-                  rounded
-                "
+                class="block w-full py-1.5 px-3 text-gray-800
+                 border border-gray-300 transition duration-500
+                 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Email"
               />
             </div>
             <!-- Password -->
             <div class="mb-3">
-              <label class="inline-block mb-2">Password</label>
+              <label class="inline-block mb-2"
+                >Password</label
+              >
               <input
                 type="password"
-                class="
-                  block
-                  w-full
-                  py-1.5
-                  px-3
-                  text-gray-800
-                  border border-gray-300
-                  transition
-                  duration-500
-                  focus:outline-none focus:border-black
-                  rounded
-                "
+                class="block w-full py-1.5 px-3 text-gray-800
+                 border border-gray-300 transition duration-500
+                 focus:outline-none focus:border-black rounded"
                 placeholder="Password"
               />
             </div>
             <button
               type="submit"
-              class="
-                block
-                w-full
-                bg-purple-600
-                text-white
-                py-1.5
-                px-3
-                rounded
-                transition
-                hover:bg-purple-700
-              "
+              class="block w-full bg-purple-600 text-white py-1.5 px-3
+               rounded transition hover:bg-purple-700"
             >
               Submit
             </button>
           </form>
           <!-- Registration Form -->
-          <form v-show="tab === 'register'">
+          <vee-form
+            v-show="tab === 'register'"
+            :validation-schema="schema"
+          >
             <!-- Name -->
             <div class="mb-3">
-              <label class="inline-block mb-2">Name</label>
-              <input
-                type="text"
-                class="
-                  block
-                  w-full
-                  py-1.5
-                  px-3
-                  text-gray-800
-                  border border-gray-300
-                  transition
-                  duration-500
-                  focus:outline-none focus:border-black
-                  rounded
-                "
+              <label class="inline-block mb-2"
+                >Name</label
+              >
+              <vee-field
+                type="text" name="name"
+                class="block w-full py-1.5 px-3 text-gray-800 border
+                 border-gray-300 transition duration-500
+                 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+              <ErrorMessage class="text-red-600" name="name" />
             </div>
             <!-- Email -->
             <div class="mb-3">
-              <label class="inline-block mb-2">Email</label>
+              <label class="inline-block mb-2"
+                >Email</label
+              >
               <input
                 type="email"
-                class="
-                  block
-                  w-full
-                  py-1.5
-                  px-3
-                  text-gray-800
-                  border border-gray-300
-                  transition
-                  duration-500
-                  focus:outline-none focus:border-black
-                  rounded
-                "
+                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300
+                 transition duration-500 focus:outline-none
+                 focus:border-black rounded"
                 placeholder="Enter Email"
               />
             </div>
             <!-- Age -->
             <div class="mb-3">
-              <label class="inline-block mb-2">Age</label>
+              <label class="inline-block mb-2"
+                >Age</label
+              >
               <input
                 type="number"
-                class="
-                  block
-                  w-full
-                  py-1.5
-                  px-3
-                  text-gray-800
-                  border border-gray-300
-                  transition
-                  duration-500
-                  focus:outline-none focus:border-black
-                  rounded
-                "
+                class="block w-full py-1.5 px-3 text-gray-800
+                 border border-gray-300 transition duration-500
+                 focus:outline-none focus:border-black rounded"
               />
             </div>
             <!-- Password -->
             <div class="mb-3">
-              <label class="inline-block mb-2">Password</label>
+              <label class="inline-block mb-2"
+                >Password</label
+              >
               <input
                 type="password"
-                class="
-                  block
-                  w-full
-                  py-1.5
-                  px-3
-                  text-gray-800
-                  border border-gray-300
-                  transition
-                  duration-500
-                  focus:outline-none focus:border-black
-                  rounded
-                "
+                class="block w-full py-1.5 px-3 text-gray-800
+                 border border-gray-300 transition duration-500 focus:outline-none
+                 focus:border-black rounded"
                 placeholder="Password"
               />
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
-              <label class="inline-block mb-2">Confirm Password</label>
+              <label class="inline-block mb-2"
+                >Confirm Password</label
+              >
               <input
                 type="password"
-                class="
-                  block
-                  w-full
-                  py-1.5
-                  px-3
-                  text-gray-800
-                  border border-gray-300
-                  transition
-                  duration-500
-                  focus:outline-none focus:border-black
-                  rounded
-                "
+                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300
+                 transition duration-500 focus:outline-none
+                 focus:border-black rounded"
                 placeholder="Confirm Password"
               />
             </div>
             <!-- Country -->
             <div class="mb-3">
-              <label class="inline-block mb-2">Country</label>
+              <label class="inline-block mb-2"
+                >Country</label
+              >
               <select
-                class="
-                  block
-                  w-full
-                  py-1.5
-                  px-3
-                  text-gray-800
-                  border border-gray-300
-                  transition
-                  duration-500
-                  focus:outline-none focus:border-black
-                  rounded
-                "
+                class="block w-full py-1.5 px-3 text-gray-800
+                 border border-gray-300 transition duration-500
+                 focus:outline-none focus:border-black rounded"
               >
                 <option value="USA">USA</option>
-                <option value="Mexico">Mexico</option>
-                <option value="Germany">Germany</option>
+                <option value="Mexico">
+                  Mexico
+                </option>
+                <option value="Germany">
+                  Germany
+                </option>
               </select>
             </div>
             <!-- TOS -->
@@ -266,25 +207,18 @@
                 type="checkbox"
                 class="w-4 h-4 float-left -ml-6 mt-1 rounded"
               />
-              <label class="inline-block">Accept terms of service</label>
+              <label class="inline-block"
+                >Accept terms of service</label
+              >
             </div>
             <button
               type="submit"
-              class="
-                block
-                w-full
-                bg-purple-600
-                text-white
-                py-1.5
-                px-3
-                rounded
-                transition
-                hover:bg-purple-700
-              "
+              class="block w-full bg-purple-600 text-white py-1.5 px-3
+               rounded transition hover:bg-purple-700"
             >
               Submit
             </button>
-          </form>
+          </vee-form>
         </div>
       </div>
     </div>
@@ -299,6 +233,15 @@ export default {
   data() {
     return {
       tab: 'login',
+      schema: {
+        name: 'required',
+        email: '',
+        age: '',
+        password: '',
+        confirm_password: '',
+        country: '',
+        tos: '',
+      },
     };
   },
   computed: {
