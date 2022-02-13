b70de0d786588f03c4a07f1ee14732a2ab2c7e7a
Thanoskaloudis <8anos.kal@gmail.com>
2022-02-13 18:11:56 +0000

    update links(adding new links, aply active class)

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
+import Manage from '@/views/Manage.vue';
 
 const routes = [
   {
+    name: 'home',
     path: '/',
     component: Home,
   },
   {
-    path: '/About',
+    name: 'about',
+    path: '/about',
     component: About,
+  },
+  {
+    name: 'manage',
+    path: '/manage-music',
+    component: Manage,
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
 });
 
 export default router;
