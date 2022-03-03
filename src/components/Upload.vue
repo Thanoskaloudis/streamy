22e35c793e9dcac11805c08e2c710fed0372a769
Thanoskaloudis <8anos.kal@gmail.com>
2022-03-03 16:00:46 +0000

    handling errors and success when uploading a file

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -47,7 +47,7 @@
 </template>
 
 <script>
-import { storage } from '@/includes/firebase';
+import { storage, auth, songsCollection } from '@/includes/firebase';
 
 export default {
   name: 'Upload',
@@ -84,6 +84,27 @@ export default {
         task.on('state_changed', (snapshot) => {
           const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
           this.uploads[uploadIndex].current_progress = progress;
+        }, (error) => {
+          this.uploads[uploadIndex].variant = 'bg-red-400';
+          this.uploads[uploadIndex].icon = 'fas fa-times';
+          this.uploads[uploadIndex].text_class = 'text-red-400';
+          console.log(error);
+        }, async () => {
+          const song = {
+            uid: auth.currentUser.uid,
+            display_name: auth.currentUser.displayName,
+            original_name: task.snapshot.ref.name,
+            modified_name: task.snapshot.ref.name,
+            genre: '',
+            comment_count: 0,
+          };
+
+          song.url = await task.snapshot.ref.getDownloadURL();
+          await songsCollection.add(song);
+
+          this.uploads[uploadIndex].variant = 'bg-green-400';
+          this.uploads[uploadIndex].icon = 'fas fa-check';
+          this.uploads[uploadIndex].text_class = 'text-green-400';
         });
       });
 
