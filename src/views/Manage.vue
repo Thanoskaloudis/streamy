53422c84bf866ed744f814d0f4d37d829e53e23e
Thanoskaloudis <8anos.kal@gmail.com>
2022-03-15 12:32:24 +0000

    validate and edit songs

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -14,7 +14,10 @@
             ></i>
           </div>
           <div class="p-6">
-            <composition-item  v-for="song in songs" :key="song.docID" />
+            <composition-item v-for="(song, i) in songs" :key="song.docID"
+            :song="song"
+            :updateSong="updateSong"
+            :index="i" />
           </div>
         </div>
       </div>
@@ -47,6 +50,12 @@ export default {
 
       this.songs.push(song);
     });
+  },
+  methods: {
+    updateSong(i, values) {
+      this.songs[i].modified_name = values.modified_name;
+      this.songs[i].genre = values.genre;
+    },
   },
   // beforeRouteLeave(to, from, next) {
   //   this.$refs.upload.cancelUploads();
